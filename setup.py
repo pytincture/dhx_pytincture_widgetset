4d17f5a0c433b6c5b30e4669abd27f11387db7bb
schapman1974 <schapman1974@gmail.com>
2024-04-03 14:28:32 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='dhxpyt',
-    version='0.4.5',
+    version='0.4.6',
     description=(
         'DHTMLX widgetset'
     ),
