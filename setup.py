140e14b45add4e8a159707017f6f4a68cfeae007
davidburroughs <dburroughs@rocketdocs.com>
2024-06-21 11:33:00 -0400

    version update

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='dhxpyt',
-    version='0.4.6',
+    version='0.4.7',
     description=(
         'DHTMLX widgetset'
     ),
