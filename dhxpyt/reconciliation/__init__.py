8d641c46c1e9cfa2378947d56f0359f19be0d993
grego33 <mattgregory33@gmail.com>
2025-02-04 14:48:04 -0500

    add Reconciliation widget and configuration classes

---
+++ dhxpyt/reconciliation/__init__.py
@@ -0,0 +1,4 @@
+from .reconciliation import Reconciliation
+from .reconciliation_config import ReconciliationConfig
+
+__all__ = ["Reconciliation", "ReconciliationConfig"]
---
+++ dhxpyt/reconciliation/reconciliation.py
@@ -0,0 +1,46 @@
+import json
+from typing import Any, Callable, Dict, List, Union
+from pyodide.ffi import create_proxy
+import js
+
+from .reconciliation_config import ReconciliationConfig
+
+class Reconciliation:
+    """
+    Wrapper class for the Reconciliation widget.
+    """
+    def __init__(self, config: ReconciliationConfig, container: str = None):
+        """
+        Initializes the Reconciliation widget.
+
+        :param config: ReconciliationConfig object containing the configuration.
+        :param container: ID of the HTML element to attach the Reconciliation to.
+        """
+        config_dict = config.to_dict()
+        self.reconciliation = js.reconciliation.Reconciliation.new(container, js.JSON.parse(json.dumps(config_dict)))
+
+    """ Reconciliation API Methods """
+
+    def set_theme(self, theme: str) -> None:
+        self.reconciliation.setTheme(js.JSON.parse(json.dumps({"name": theme, "fonts": True})))
+
+    def export_to_json(self) -> str:
+        """Exports Reconciliation data to JSON format."""
+        return self.reconciliation.data.serialize()
+
+    """ Reconciliation Event Handlers """
+
+    def add_event_handler(self, event_name: str, handler: Callable) -> None:
+        """Adds an event handler for the specified event."""
+        event_proxy = create_proxy(handler)
+        self.reconciliation.events.on(event_name, event_proxy)
+
+    # def on_card_click(self, handler: Callable[[Dict[str, Any]], None]) -> None:
+    #     """Fires when a card is clicked."""
+    #     def event_handler(card):
+    #         handler(card.to_py())
+    #     self.reconciliation.events.on("cardClick", create_proxy(event_handler))
+
+        #TODO:
+        # add transaction
+        # 
