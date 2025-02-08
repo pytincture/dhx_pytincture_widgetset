e2bc8a68e27bb3eabb0bf1eb64bf9ea76accc32d
grego33 <mattgregory33@gmail.com>
2025-02-08 18:46:29 -0500

    redo grid show/hide working

--- dhxpyt/dhxsrc/reconciliation.js
+++ dhxpyt/dhxsrc/reconciliation.js
@@ -28,7 +28,7 @@
                   {
                     type: "none",
                     padding: 10,
-                    rows: this.initCategoryLayout(left_rows)
+                    rows: this.setupCategoryLayout(left_rows)
                   }
                 ]
               },
@@ -38,7 +38,7 @@
                   {
                     type: "none",
                     padding: 10,
-                    rows: this.initCategoryLayout(right_rows)
+                    rows: this.setupCategoryLayout(right_rows)
                   }
                 ]
               }
@@ -46,6 +46,18 @@
           }
         ]
       });
+
+      const currentLayout = this.layout;
+      currentLayout.events.on("afterShow", (cellId) => {
+        console.log(`layout afterShow ${cellId}`);
+        console.log(left_rows);
+        console.log(right_rows);
+        const categoryId = cellId.replace("_items", "");
+        const category = left_rows.find(cat => cat.category_id === categoryId) ||
+                 right_rows.find(cat => cat.category_id === categoryId);
+        currentLayout.getCell(cellId).attach(this.buildItemsGrid(category));
+      });
+
       container.attach(this.layout);
 
       // var toolbar_h1 = new dhx.Toolbar(null, { css: "dhx_widget--bordered" }); toolbar_h1.data.parse([{}]); this.layout.getCell("H1").attach(toolbar_h1);
@@ -68,36 +80,40 @@
       return `${category_id}_header`;
     }
 
+    _getCategoryItemsId(category_id) {
+      return `${category_id}_items`;
+    }
+
     _computeCategoryTotal(category) {
       return category.transactions.reduce((acc, item) => acc + item.amount, 0);
     }
 
-    initCategoryLayout(rows) {
+    setupCategoryLayout(rows) {
       // Initialize an empty array
       const list = [];
       console.log(rows)
       for (const row of rows) {
         const header_id = this._getCategoryHeaderId(row.category_id);
-        const items_id = `${row.category_id}_items`;
+        const items_id = this._getCategoryItemsId(row.category_id);
         console.log(`creating header id ${header_id}`);
         console.log(`creating items id ${items_id}`);
 
-        var category_header = {
+        var category_header_cell = {
           id: header_id,
           height: "auto",
           padding: 5,
         };
 
-        var category_items = {
+        var category_items_cell = {
           id: items_id,
           height: "auto",
           hidden: true,
-          // html: `<div id="${row.category_id}_grid"></div>`,
+          html: `<h2>Hi</h2>`,
         };
 
         // Add the object to the list
-        list.push(category_header);
-        list.push(category_items);
+        list.push(category_header_cell);
+        list.push(category_items_cell);
       }
 
       return list;
@@ -122,21 +138,60 @@
         const header_cell_id = this._getCategoryHeaderId(category_id);
         const categoryToolbar = new dhx.Toolbar(null, { css: "dhx_widget--bordered" });
         categoryToolbar.data.parse(this.getHeaderToolbarConfig(toolbar_id, row));
+        this.addCategoryHeaderEvents(categoryToolbar, this.layout);
         this.layout.getCell(header_cell_id).attach(categoryToolbar);
         this.toolbars[toolbar_id] = categoryToolbar;
       }
     }
 
-    
-    populateCategories(rows) {
-      for (let i = 0; i < rows.length; i++) {
-        const row = rows[i];
-        const item_details_grid = this.buildItemsGrid(row);
-        console.log(`Attaching grid ${item_details_grid.data} to ${row.category_id}_items`);
-        this.layout.getCell(`${row.category_id}_items`).attach(item_details_grid);
-      }
+    addCategoryHeaderEvents(categoryToolbar, currentLayout) {
+      categoryToolbar.events.on("click", function (id, e) {
+        if (id.endsWith("up")) {
+          // -2 because of up
+          const category_toolbar_id = id.slice(0, -2);
+          const down_id = `${category_toolbar_id}down`;
+          categoryToolbar.hide([id]);
+          categoryToolbar.show([down_id]);
+          const category_items_id = category_toolbar_id.replace("_toolbar", "_items");
+          currentLayout.cell(`${category_items_id}`).show();
+        } else {
+          // -4 is becuase of down
+          const category_toolbar_id = id.slice(0, -4);
+          const up_id = `${category_toolbar_id}up`;
+          const items_id = `${id.slice(0, -11)}_items`;
+          categoryToolbar.show([up_id]);
+          categoryToolbar.hide([id]);
+          const category_items_id = category_toolbar_id.replace("_toolbar", "_items");
+          currentLayout.cell(`${category_items_id}`).hide();
+        }
+      });
     }
-  
+
+    
+    buildItemsGrid(category) {
+      console.log(`building grid for ${category}`);
+      const dataset = category.transactions.map(({ memo, amount }) => {
+        return {
+          "selected": "true",
+          "info": `${memo}`,
+          "total": `${amount}`
+        };
+      });
+
+      console.log(`building grid for ${category.category_id}_grid using ${dataset}`);
+      const grid = new dhx.Grid(`${category.category_id}_grid`, {
+        columns: [
+          { id: "selected", header: [{ text: "" }] },
+          { id: "info", header: [{ text: "Info" }] },
+          { id: "total", header: [{ text: "Amount" }] },
+        ],
+        headerRowHeight: 0,
+        data: dataset,
+        align: "left",
+      });
+
+      return grid;
+    }  
   }
 
   // Make Reconciliation class available globally:
