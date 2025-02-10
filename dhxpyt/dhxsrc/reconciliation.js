b0fe39669ffa0d0970187de0362b9df95de5b0bb
grego33 <mattgregory33@gmail.com>
2025-02-09 20:29:15 -0500

    grid hide/show works and headers hidden

--- dhxpyt/dhxsrc/reconciliation.js
+++ dhxpyt/dhxsrc/reconciliation.js
@@ -47,29 +47,29 @@
         ]
       });
 
-      const currentLayout = this.layout;
-      currentLayout.events.on("afterShow", (cellId) => {
+      this.layout.events.on("afterShow", (cellId) => {
         console.log(`layout afterShow ${cellId}`);
         console.log(left_rows);
         console.log(right_rows);
         const categoryId = cellId.replace("_items", "");
+        console.log(`searching for category ${categoryId}`);
         const category = left_rows.find(cat => cat.category_id === categoryId) ||
                  right_rows.find(cat => cat.category_id === categoryId);
-        currentLayout.getCell(cellId).attach(this.buildItemsGrid(category));
+        
+        console.log(`passing category ${category.category_id}`);
+        this.layout.getCell(cellId).attach(this.buildItemsGrid(category));
       });
 
       container.attach(this.layout);
 
-      // var toolbar_h1 = new dhx.Toolbar(null, { css: "dhx_widget--bordered" }); toolbar_h1.data.parse([{}]); this.layout.getCell("H1").attach(toolbar_h1);
-
-      // this.populateCategories(left_rows);
-      // this.populateCategories(right_rows);
-      
-      // this.makeHeaderSection(left_rows);
-      // this.makeHeaderSection(right_rows);
-
       this.renderCategoryHeaders(left_rows);
       this.renderCategoryHeaders(right_rows);
+
+      dhx.cssManager.add({
+        display: "none",
+        padding: 0
+      }, "dhx_grid-header-cell");
+
     }
 
     _getToolbarId(category_id) {
@@ -169,7 +169,8 @@
 
     
     buildItemsGrid(category) {
-      console.log(`building grid for ${category}`);
+      console.log(`building grid for ${category.category_id}`);
+      console.log(`category.transactions ${JSON.stringify(category)}`);
       const dataset = category.transactions.map(({ memo, amount }) => {
         return {
           "selected": "true",
@@ -178,18 +179,19 @@
         };
       });
 
-      console.log(`building grid for ${category.category_id}_grid using ${dataset}`);
+      console.log(`building grid for ${category.category_id}_grid using ${JSON.stringify(dataset)}`);
       const grid = new dhx.Grid(`${category.category_id}_grid`, {
         columns: [
-          { id: "selected", header: [{ text: "" }] },
-          { id: "info", header: [{ text: "Info" }] },
-          { id: "total", header: [{ text: "Amount" }] },
+          { id: "selected", width: "auto", header: [{ text: "" }] },
+          { id: "info", width: "auto", header: [{ text: "Info" }] },
+          { id: "total", width: "auto", header: [{ text: "Amount" }] },
         ],
         headerRowHeight: 0,
-        data: dataset,
-        align: "left",
+        adjust: true,
+        autoWidth: true
       });
 
+      grid.data.parse(dataset);
       return grid;
     }  
   }
