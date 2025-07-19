f74c75b8138595e3a21c6a4a683a7e7ab917897b
schapman1974 <schapman1974@gmail.com>
2025-07-18 21:19:32 -0400

    Update __init__.py

--- dhxpyt/__init__.py
+++ dhxpyt/__init__.py
@@ -1,4 +1,4 @@
 __widgetset__ = "dhxpyt"
-__version__ = "0.7.7"
+__version__ = "0.7.8"
 __version_tuple__ = tuple(map(int, __version__.split('.')))
 __description__ = "Python wrapper for DHTMLX widgets"
